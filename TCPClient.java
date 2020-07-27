abb9f1847be6a7dcdb1a3ff7ec0ddea579d60ba3
mmmm <49637356+hlj6040@users.noreply.github.com>
2020-07-27 22:07:55 +0800

    Add files via upload

---
+++ TCPClient.java
@@ -0,0 +1,24 @@
+package project;
+import  java.io.*;
+import  java.net.*;
+
+public class TCPClient {
+
+	public static void main(String[] args) throws Exception
+	{
+		
+		String sentence;
+		String modifiedSentence;
+		BufferedReader  inFromUser = new BufferedReader( new InputStreamReader(System.in)); 
+		Socket clientSocket = new Socket("182.92.64.184",8081); 
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader( new InputStreamReader(clientSocket.getInputStream()));
+		sentence = inFromUser.readLine();
+		outToServer.writeBytes(sentence + '\n');
+		modifiedSentence = inFromServer.readLine();
+		System.out.print("FROM SERVER:" + modifiedSentence);
+		clientSocket.close();
+	
+		
+	}
+}
---
+++ TCPServer.java
@@ -0,0 +1,20 @@
+package project;
+import java.net.*;
+import java.io.*;
+
+class TCPServer {
+	public static void main(String argv[])  throws  Exception
+	{
+		String clientSentence;
+		String capitalizedSentence;
+		ServerSocket welcomSocket = new ServerSocket(6789);
+		while(true) {
+			Socket connectionSocket = welcomSocket.accept();
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			clientSentence = inFromClient.readLine();
+			capitalizedSentence = clientSentence.toUpperCase() +'\n';
+			outToClient.writeBytes(capitalizedSentence);
+		}
+	}
+}
